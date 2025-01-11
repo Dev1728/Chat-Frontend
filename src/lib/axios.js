5e770ee79b2588f5f51118e790800407a0d0a781
Devesh Dipak Jadhav <dj64360@hmai.com>
2025-01-11 14:00:18 +0530

    Frontend url upt

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "production" ? "https://chat-backend-3v52.onrender.com/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-backend-3v52.onrender.com/api" : "/api",
   withCredentials: true,
 });
